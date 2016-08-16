0550eca00fe48648580a111e9d0c76bc95d7cbd4
James Kozianski <soulplant@gmail.com>
2016-08-17 07:04:40 +1000

    Converted ListMaker into a class.

--- client/src/listmaker.ts
+++ client/src/listmaker.ts
@@ -4,65 +4,74 @@ import { Ingredient, IngredientList, Quantity, Section, Recipe } from './types';
 import { parseSections, parseRecipes } from './parser';
 
 
-export function ListMaker(recipes, aislesText) {
-  this.recipes = recipes;
-  this.aisles = {};
-  var sections = parseSections(aislesText);
-  this.aisleNames = sections.map(function(section) { return section.header; });
-  this.aisleNames.push('Other');
+export class ListMaker {
+  recipes: Recipe[];
+  aisleNames: string[];
 
-  var self = this;
+  // What aisles ingredients are in.
+  aisles: {[ingredient: string]: string};
 
-  sections.forEach(function(section) {
-    section.parts[0].forEach(function(ingredient) {
-      self.aisles[ingredient] = section.header;
+  constructor(recipes: Recipe[], aislesText: string) {
+    this.recipes = recipes;
+    var sections = parseSections(aislesText);
+    this.aisleNames = sections.map(function(section) { return section.header; });
+    this.aisleNames.push('Other');
+    this.aisles = {};
+
+    var self = this;
+
+    sections.forEach(function(section) {
+      section.parts[0].forEach(function(ingredient) {
+        self.aisles[ingredient] = section.header;
+      });
     });
-  });
-}
+  }
 
-ListMaker.prototype.makeList = function(ingredients: any[]) {
-  var byAisle: {[name: string]: any[]} = {};
-  for (var i = 0; i < ingredients.length; i++) {
-    var aisle = this.aisles[ingredients[i].name];
-    if (!aisle) {
-      aisle = 'Other';
+  // TODO: Fix the types here.
+  makeList(ingredients: any[]): any[] {
+    var byAisle: {[name: string]: any[]} = {};
+    for (var i = 0; i < ingredients.length; i++) {
+      var aisle = this.aisles[ingredients[i].name];
+      if (!aisle) {
+        aisle = 'Other';
+      }
+      if (!byAisle[aisle]) {
+        byAisle[aisle] = [];
+      }
+      byAisle[aisle].push(ingredients[i]);
     }
-    if (!byAisle[aisle]) {
-      byAisle[aisle] = [];
-    }
-    byAisle[aisle].push(ingredients[i]);
-  }
-  var results = [];
-  for (var i = 0; i < this.aisleNames.length; i++) {
-    var aisle = this.aisleNames[i];
-    var ingredientsInAisle = byAisle[aisle];
-    if (ingredientsInAisle) {
-      ingredientsInAisle.sort(function(l, r) {
-        if (l.name < r.name) {
-          return 0;
-        } else if (l.name == r.name) {
-          return 0;
-        } else {
-          return 1;
-        }
-      });
-      results.push({name: aisle, header: true});
-      var self = this;
-      ingredientsInAisle.forEach(function(i) {
-        var shortNames = i.recipes.map(function(recipeId) {
-          return getShortName(self.recipes[recipeId].name);
+    var results = [];
+    for (var i = 0; i < this.aisleNames.length; i++) {
+      var aisle = this.aisleNames[i];
+      var ingredientsInAisle = byAisle[aisle];
+      if (ingredientsInAisle) {
+        ingredientsInAisle.sort(function(l, r) {
+          if (l.name < r.name) {
+            return 0;
+          } else if (l.name == r.name) {
+            return 0;
+          } else {
+            return 1;
+          }
         });
-        i.note = '';
-        if (shortNames.length > 0) {
-          i.note = shortNames.join(', ');
-        }
-      });
-      results.push.apply(results, ingredientsInAisle);
+        results.push({name: aisle, header: true});
+        var self = this;
+        ingredientsInAisle.forEach(function(i) {
+          var shortNames = i.recipes.map(function(recipeId) {
+            return getShortName(self.recipes[recipeId].name);
+          });
+          i.note = '';
+          if (shortNames.length > 0) {
+            i.note = shortNames.join(', ');
+          }
+        });
+        results.push.apply(results, ingredientsInAisle);
+      }
     }
-  }
-  // Array.<{name: string, header: ?bool, quantities: ?Array<{0: int, 1: string}>}>
-  return results;
-};
+    // Array.<{name: string, header: ?bool, quantities: ?Array<{0: int, 1: string}>}>
+    return results;
+  };
+}
 
 function allCaps(str: string): boolean {
   for (var i = 0; i < str.length; i++) {
